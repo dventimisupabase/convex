f75d7026899572db4963e71edccce9acaa1c3b36
David A. Ventimiglia <david.ventimiglia@supabase.io>
2025-07-07 12:40:06 -0700

    Fixing up the migration scripts

--- supabase/migrations/20250701205606_create_core_build_llm_prompt_function.sql
+++ supabase/migrations/20250701205606_create_core_build_llm_prompt_function.sql
@@ -41,18 +41,20 @@ create or replace function core.build_llm_prompt(t core.ticket, k int default 10
 
 You are a customer support engineer.  Your job is to help customers solve their issues by writing helpful, accurate, and timely responses, or internal comments to coordinate with colleagues.  First, you will be presented with a NEW SUPPORT TICKET.  Second, you will be presented with a series of PAST RESOLVED TICKETS which may offer relevant language, troubleshooting steps, or internal advice for dealing with the NEW SUPPORT TICKET.
 
-Please generate the next OUTTBOUND message for the NEW SUPPORT TICKET based on experience from PAST RESOLVED TICKETS.
+Please generate the next OUTTBOUND message for the NEW SUPPORT TICKET based on experience from PAST RESOLVED TICKETS.  This document and all of the related content is organized in Markdown format, and each TICKET in the series of PAST RESOLVED TICKETS is on its own separate Markdown document.  Note that the PAST RESOLVED SUPPORT tickets are likely to contain personalized signatures from support engineers.  When generating the next response, use a template <INSERT NAME HERE> in the signature instead of using an existing support engineer name.
 
 ## NEW SUPPORT TICKET
 %s
 
 ## PAST RESOLVED TICKETS
+
+---
 %s
 $$,
-core.format_ticket_for_prompt(t),
+(core.format_ticket_for_prompt(t)).formatted,
 (
   select
-    string_agg((select formatted from core.format_ticket_for_embedding(st)), E'\n---\n')
+    string_agg((select formatted from core.format_ticket_for_prompt(st)), E'\n---\n')
     from
       core.search_tickets(t, k) st
    where
